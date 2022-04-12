1bb8bf8048c5beb378d2212a6c7fb70254eadd22
shaho1090 <shaho.parvini@gmail.com>
2022-04-12 22:57:17 +0430

    pass assignment for new fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'phone_number',
+        'type',
         'email',
         'password',
     ];
