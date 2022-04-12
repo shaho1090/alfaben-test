9590abc386eb53de4fec1a82f180a30af013c1a6
shaho1090 <shaho.parvini@gmail.com>
2022-04-12 22:56:36 +0430

    add more fields for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('surname');
+            $table->string('phone_number')->unique();
+            $table->string('type');
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
