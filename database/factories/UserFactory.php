7ab43b13f358fe5516fc08a31b9c0828dc24e4d8
shaho1090 <shaho.parvini@gmail.com>
2022-04-12 22:58:19 +0430

    complete user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Types\PassengerTypes;
+use App\Types\UserTypes;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -14,11 +16,17 @@ class UserFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws \ReflectionException
      */
     public function definition()
     {
+        $passengerTypes = (new PassengerTypes())->toArray();
+
         return [
             'name' => $this->faker->name(),
+            'surname' => $this->faker->lastName(),
+            'phone_number' => (string)rand(1000000000,9999999999),
+            'type' => $passengerTypes[array_rand($passengerTypes)],
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -39,4 +47,27 @@ class UserFactory extends Factory
             ];
         });
     }
+
+    public function passenger(): UserFactory
+    {
+        return $this->state(function (array $attributes) {
+
+            $passengerTypes = (new PassengerTypes())->toArray();
+
+            return [
+                'type' => $passengerTypes[array_rand($passengerTypes)],
+            ];
+        });
+    }
+
+    public function clerk(): UserFactory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => UserTypes::CLERK,
+            ];
+        });
+    }
+
+
 }
